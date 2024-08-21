a39128be4c6e6918207abc7b0f51bb3d0107d2db
MsSuraj001 <milukumar225@gmail.com>
2024-08-21 14:47:51 +0530

    start at the ecomm project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Layout from './Layout/Layout'
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <h1>this is the app</h1>
+     </Layout>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+
+function Home() {
+  return (
+    <Layout>
+      <h1>this is the home page</h1>
+    </Layout>
+  )
+}
+
+export default Home
