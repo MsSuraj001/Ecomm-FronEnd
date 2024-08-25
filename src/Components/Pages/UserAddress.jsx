e32eb4a23680a7dfe17c7b19b0488fee62d4bb61
MsSuraj001 <milukumar225@gmail.com>
2024-08-25 11:31:55 +0530

    added the address functionality

--- src/Components/Pages/UserAddress.jsx
+++ src/Components/Pages/UserAddress.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../Layout/Layout'
 import Home from '../../assets/svg Icons/Home'
 import Work from '../../assets/svg Icons/Work'
 
 function UserAddress() {
+  const [userAddressState, setUserAddressState] = useState({
+      FullName : '',
+      PhoneNumber: '',
+      State : '',
+      City : '',
+      PinCode : '',
+      LandMark : '',
+      RoadName : '',
+  })
+
+  function handleUserInput(e){
+    const {name, value} = e.target
+    setUserAddressState({
+      ...userAddressState,
+      [name] : value
+    })
+  }
+
+  function handleUserSubmit(e){
+    e.preventDefault()
+    console.log(userAddressState)
+  }
   return (
     // <Layout>
       <div className='w-full h-full flex justify-center items-center'>
@@ -11,46 +33,60 @@ function UserAddress() {
             <form className='w-full py-4 px-4 '>
                 <h1 className='text-center font-bold text-2xl pb-4'>Your Address</h1>
 
-                <lable className="">Full Name <span className='text-red-800'>*</span></lable>
+                <label>Full Name <span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='FullName'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your full name"
                 />
 
-                <lable className="">Phone Number<span className='text-red-800'>*</span></lable>
+                <label>Phone Number<span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='PhoneNumber'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your Post office"/>
 
-                <lable className="">State <span className='text-red-800'>*</span></lable>
+                <label>State <span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='State'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your State"/>
 
-                <lable className="">City<span className='text-red-800'>*</span></lable>
+                <label>City<span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='City'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your City"/>
 
-                <lable className="">Pin Code <span className='text-red-800'>*</span></lable>
+                <label>Pin Code <span className='text-red-800'>*</span></label>
                 <input 
                     type="Number" 
+                    name='PinCode'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your Pin Code"/>
 
-                <lable className="">Land Mark<span className='text-red-800'>*</span></lable>
+                <label>Land Mark<span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='LandMark'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your Lank Mark"/>
 
-                <lable className="">Road name, Area, Colony<span className='text-red-800'>*</span></lable>
+                <label>Road name, Area, Colony<span className='text-red-800'>*</span></label>
                 <input 
                     type="text" 
+                    name='RoadName'
+                    onChange={handleUserInput}
                     className="w-full p-2 mb-4 border" 
                     placeholder="Please enter your Road Name, Area, Colony"/>
 
@@ -68,7 +104,7 @@ function UserAddress() {
                   </div>
                 </div>
 
-                <button className='bg-blue-600 py-3 px-11 rounded-md mb-0 mt-4 w-full text-white'>Save Address</button>
+                <button className='bg-blue-600 py-3 px-11 rounded-md mb-0 mt-4 w-full text-white' onClick={handleUserSubmit}>Save Address</button>
             </form>
         </div>
       </div>
